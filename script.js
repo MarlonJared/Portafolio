f5fc4a0a571485c2163567560997848538bbb11a
MarlonJared <jaredospina2013@gmail.com>
2025-02-03 23:11:01 +0100

    vr03
    subida de elementos y redes sociales

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-// Ejemplo de interactividad
-document.addEventListener('DOMContentLoaded', function() {
-    // Aquí puedes agregar cualquier funcionalidad que necesites
-    console.log('La página ha cargado correctamente');
+document.addEventListener("DOMContentLoaded", function () {
+  const svg = document.querySelector("svg");
+  const pointLight = document.querySelector("#point-light");
+
+  function handleMove(event) {
+      const x = event.clientX || event.touches?.[0]?.clientX;
+      const y = event.clientY || event.touches?.[0]?.clientY;
+
+      if (x && y) {
+          pointLight.setAttribute("x", x  );
+          pointLight.setAttribute("y", y -100);
+      }
+  }
+
+  svg.addEventListener("mousemove", handleMove);
+  svg.addEventListener("touchmove", handleMove);
 });
